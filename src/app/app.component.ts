17505c0b738cc5fd1418eb82df58b4992dcad184
Sebastián Aguilar <al221810693@gmail.com>
2022-01-06 13:09:19 -0600

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-personas';
+
+  title = 'listado de personas';
+  nombreInput: string = '';
+  apellidoInput: string = '';
+
+  personas: Persona[] = [
+                          new Persona('Juan', 'Perez'), 
+                          new Persona('Laura', 'Juarez'),
+                          new Persona('Violet', 'Evergarden')
+                        ];
+
+  agregarPersona(){
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona1);
+  }
 }
